6dcc781da6717f46cd82f2f29543c57b447cf2b4
AD7six <andydawson76@yahoo.co.uk>
2010-04-07 14:56:46 +0200

    remove unnecessary function

--- mi_crawler.php
+++ mi_crawler.php
@@ -12,32 +12,6 @@ if (!function_exists('getMicrotime')) {
 	}
 }
 
-if (!function_exists('debug')) {
-/**
- * debug method
- *
- * @param bool $var false
- * @param bool $showHtml false
- * @param bool $showFrom true
- * @return void
- * @access public
- */
-	function debug($var = false, $showHtml = false, $showFrom = true) {
-		if ($showFrom) {
-			$calledFrom = debug_backtrace();
-			echo '<strong>' . substr(str_replace(dirname(dirname(__FILE__)), '', $calledFrom[0]['file']), 1) . '</strong>';
-			echo ' (line <strong>' . $calledFrom[0]['line'] . '</strong>)';
-		}
-		echo "\n<pre class=\"debug\">\n";
-
-		$var = print_r($var, true);
-		if ($showHtml) {
-			$var = str_replace('<', '&lt;', str_replace('>', '&gt;', $var));
-		}
-		echo $var . "\n</pre>\n";
-	}
-}
-
 /**
  * MiCrawler class
  *
